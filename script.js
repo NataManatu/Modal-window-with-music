a1182d2eed4e583fd204294ed422b61418ae8b26
NataManatu <vip.aleksa78@mail.ru>
2022-11-08 02:34:07 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,105 @@
+// 1.Кнопка
+// 2.Отслеживаем клик
+// 3.Затемняем экран
+// 4.Поверх темного экрана рисуем модальное окно
+// 5.В модальном окне отрисовываем разные кнопки
+// 6.Вешаем клик на каждую кнорку модального окна
+// 7.Отслеживаем клик на затемненную область
+
+
+const openModalButton = document.getElementById("open_modal");
+console.log(openModalButton)
+
+openModalButton.addEventListener("click", () => {
+    // const backdrop = document.querySelector(".backdrop");
+    // backdrop.style.display = "initial";
+
+    const wrapper = document.createElement('div');
+    wrapper.className = "modalWrapper";
+
+
+    const backdrop = document.createElement('div');
+    backdrop.className = "backdrop";
+    wrapper.appendChild(backdrop);
+
+
+
+    const modalContainer = document.createElement('div');
+    modalContainer.className = "modal";
+    wrapper.appendChild(modalContainer);
+
+
+    const cross = document.createElement('button');
+    cross.setAttribute = ("type", "button");
+    cross.className = "modalCross";
+    cross.innerText = "x"
+
+
+
+    const buttonOk = document.createElement('button');
+    buttonOk.setAttribute = ("type", "button");
+    buttonOk.className = "modalOkButton";
+    buttonOk.innerText = "Ok";
+
+
+
+    const buttonCansel = document.createElement('button');
+    buttonCansel.setAttribute = ("type", "button");
+    buttonCansel.className = "modalCanselButton";
+    buttonCansel.innerText = "Oтмена";
+
+
+
+    backdrop.addEventListener("click", closeModal)
+    cross.addEventListener("click", closeModal)
+    buttonOk.addEventListener("click", myMuse)
+    buttonCansel.addEventListener("click", myMuseStop)
+
+
+
+    modalContainer.appendChild(cross);
+    modalContainer.appendChild(buttonOk);
+    modalContainer.appendChild(buttonCansel);
+
+
+
+    document.body.appendChild(wrapper);
+
+  
+});
+
+function closeModal() {
+    const modal = document.querySelector('.modalWrapper');
+
+    if (!modal) {
+        console.error('hh');
+        return;
+    }
+    else {
+        modal.remove();
+        
+    }
+}
+
+
+const audio = new Audio();
+audio.src = "L.mp3";
+audio.autoplay = false;
+
+
+function myMuse() {
+   audio.play()
+   audio.autoplay = !audio.autoplay
+}
+
+function myMuseStop() {
+    audio.pause()
+    audio.autoplay = !audio.autoplay
+ }
+
+
+
+
+
+
+
